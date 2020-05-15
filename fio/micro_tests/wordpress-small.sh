9e041008a8bf4496a7b6c3252c44d841a759f378
Jesse Noller <jnoller@gmail.com>
2020-05-15 21:39:20 +0000

    wordpress

--- fio/micro_tests/wordpress-small.sh
+++ fio/micro_tests/wordpress-small.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 targ=$1
-cd "${targ}" || exit 1
-wget -qO- https://wordpress.org/latest.tar.gz | tar xvz -C ./wp
+wget -qO- https://wordpress.org/latest.tar.gz | tar xvz -C ${targ}
